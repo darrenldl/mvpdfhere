9c960d7c108cf7c94343b356f42964880f07dbed
Darren Ldl <darrenldldev@gmail.com>
2019-12-18 23:59:11 +1100

    Code refactoring

--- src/mvpdfhere.ml
+++ src/mvpdfhere.ml
@@ -1,6 +1,12 @@
-type yn =
+type yns =
   [ `Yes
   | `No
+  | `Stop
+  ]
+
+type action =
+  [ `Stop of string option
+  | `Retry
   ]
 
 let run_command cmd = match Sys.command cmd with 0 -> Ok () | x -> Error x
@@ -10,16 +16,17 @@ let write_text_dump ~pdf_file_path ~text_dump_path : (unit, int) result =
     (Printf.sprintf "pdftotext -f 1 -l 1 -- \"%s\" - | head -n 20 > %s"
        pdf_file_path text_dump_path)
 
-let ask_yn ~prompt : yn =
+let ask_yns ~prompt : yns =
   let rec aux prompt answer =
     match answer with
     | Some x -> x
     | None -> (
-        Printf.printf "%s y/n : " prompt;
+        Printf.printf "%s y/n/s : " prompt;
         let res = try read_line () with End_of_file -> "" in
         match res with
         | "y" -> `Yes
         | "n" -> `No
+        | "s" -> `Stop
         | _ ->
           print_endline "Invalid answer";
           aux prompt None )
@@ -74,19 +81,21 @@ let edit_loop ~pdf_file_path : (string, unit) result =
                        let name = String.concat "__" parts in
                        Printf.printf "Computed file name is : \"%s\"\n" name;
                        match
-                         ask_yn
+                         ask_yns
                            ~prompt:
                              (Printf.sprintf
                                 "Move PDF file to current dir using above name \
-                                 (n = redo, Ctrl+C to exit)?")
+                                 (n = redo, s = stop)?")
                        with
-                       | `Yes -> Ok (Some name)
-                       | `No -> Ok None ) ) ))
+                       | `Yes -> Ok (`Stop (Some name))
+                       | `Stop -> Ok (`Stop None)
+                       | `No -> Ok `Retry ) ) ))
     in
     match name with
     | Error () -> Error ()
-    | Ok None -> aux ~pdf_file_path
-    | Ok (Some x) -> Ok x
+    | Ok (`Stop None) -> Error ()
+    | Ok (`Stop (Some x)) -> Ok x
+    | Ok `Retry -> aux ~pdf_file_path
   in
   aux ~pdf_file_path
 
