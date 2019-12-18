1ba2094c647cdc262c0104414f38b6c4aebc5d44
Darren Ldl <darrenldldev@gmail.com>
2019-12-18 23:06:31 +1100

    Added info extraction, formatting

--- src/info.ml
+++ src/info.ml
@@ -6,33 +6,20 @@ type t = {
   title : string option;
 }
 
-let empty = {
-  year = None;
-  month = None;
-  day = None;
-  journal = None;
-  title = None;
-}
+let empty =
+  { year = None; month = None; day = None; journal = None; title = None }
 
 let map_int_field_to_json (x : int option) : Yojson.Basic.t =
-  match x with
-  | None -> `Null
-  | Some x -> `Int x
+  match x with None -> `Null | Some x -> `Int x
 
 let map_string_field_to_json (x : string option) : Yojson.Basic.t =
-  match x with
-  | None -> `Null
-  | Some x -> `String x
+  match x with None -> `Null | Some x -> `String x
 
 let map_int_field_from_json (x : Yojson.Basic.t) : int option =
-  match x with
-  | `Int x -> Some x
-  | _ -> None
+  match x with `Int x -> Some x | _ -> None
 
 let map_string_field_from_json (x : Yojson.Basic.t) : string option =
-  match x with
-  | `String x -> Some x
-  | _ -> None
+  match x with `String x -> Some x | _ -> None
 
 let to_json (t : t) : Yojson.Basic.t =
   let l =
@@ -50,22 +37,19 @@ let of_json (x : Yojson.Basic.t) : t =
   match x with
   | `Assoc l ->
     l
-    |> List.fold_left (fun info (k, v) ->
-        match k with
-        | "year" -> { info with year = map_int_field_from_json v}
-        | "month" -> { info with month = map_int_field_from_json v}
-        | "day" -> { info with day = map_int_field_from_json v}
-        | "journal" -> { info with journal = map_string_field_from_json v}
-        | "title" -> { info with title = map_string_field_from_json v}
-        | _ -> info
-      ) empty
+    |> List.fold_left
+      (fun info (k, v) ->
+         match k with
+         | "year" -> { info with year = map_int_field_from_json v }
+         | "month" -> { info with month = map_int_field_from_json v }
+         | "day" -> { info with day = map_int_field_from_json v }
+         | "journal" -> { info with journal = map_string_field_from_json v }
+         | "title" -> { info with title = map_string_field_from_json v }
+         | _ -> info)
+      empty
   | _ -> failwith "Unexpected case"
 
-let write ~json_path t =
-  Yojson.Basic.to_file json_path (to_json t)
+let write ~json_path t = Yojson.Basic.to_file json_path (to_json t)
 
 let load ~json_path : t =
-  try
-    Yojson.Basic.from_file json_path |> of_json
-  with
-  | _ -> empty
+  try Yojson.Basic.from_file json_path |> of_json with _ -> empty
--- src/mvpdfhere.ml
+++ src/mvpdfhere.ml
@@ -1,7 +1,14 @@
-type yn = [ `Yes | `No ]
+type yn =
+  [ `Yes
+  | `No
+  ]
 
 let write_text_dump ~pdf_file_path ~text_dump_path : (unit, int) result =
-  match Sys.command (Printf.sprintf "pdftotext -f 1 -l 1 -- \"%s\" - | head -n 20 > %s" pdf_file_path text_dump_path) with
+  match
+    Sys.command
+      (Printf.sprintf "pdftotext -f 1 -l 1 -- \"%s\" - | head -n 20 > %s"
+         pdf_file_path text_dump_path)
+  with
   | 0 -> Ok ()
   | x -> Error x
 
@@ -9,14 +16,15 @@ let ask_yn ~prompt : yn =
   let rec aux prompt answer =
     match answer with
     | Some x -> x
-    | None ->
-      Printf.printf "%s y/n : " prompt;
-      let res = try read_line () with End_of_file -> "" in
-      match res with
-      | "y" -> `Yes
-      | "n" -> `No
-      | _ -> print_endline "Invalid answer";
-        aux prompt None
+    | None -> (
+        Printf.printf "%s y/n : " prompt;
+        let res = try read_line () with End_of_file -> "" in
+        match res with
+        | "y" -> `Yes
+        | "n" -> `No
+        | _ ->
+          print_endline "Invalid answer";
+          aux prompt None )
   in
   aux prompt None
 
@@ -25,32 +33,48 @@ let edit_loop ~pdf_file_path : (string, unit) result =
     let text_dump_path = Filename.temp_file "mvpdfhere" ".txt" in
     let json_path = Filename.temp_file "mvpdfhere" ".json" in
     let name =
-    Fun.protect ~finally:(fun () ->
-        Sys.remove text_dump_path;
-        Sys.remove json_path;
-      )
-      (fun () ->
-         match write_text_dump ~pdf_file_path ~text_dump_path with
-         | Error x ->
-           Printf.printf "Text dump command exited with return code %d\n" x;
-           Error ()
-         | Ok () ->
-           Info.write ~json_path Info.empty;
-           match Sys.command (Printf.sprintf "vim -O %s %s" json_path text_dump_path) with
-           | 0 -> (
-             let info = Info.load ~json_path in
-             let parts =[
-             ] in
-             let name = String.concat "__" parts in
-             Printf.printf "Computed file name is : %s\n" name;
-             match ask_yn ~prompt:(Printf.sprintf "Move PDF file to current dir using above name?") with
-             | `Yes -> Ok (Some name)
-             | `No -> Ok None
-             )
-           | x ->
-             Printf.printf "Vim exited with return code %d\n" x;
+      Fun.protect
+        ~finally:(fun () ->
+            Sys.remove text_dump_path;
+            Sys.remove json_path)
+        (fun () ->
+           match write_text_dump ~pdf_file_path ~text_dump_path with
+           | Error x ->
+             Printf.printf "Text dump command exited with return code %d\n" x;
              Error ()
-      ) in
+           | Ok () -> (
+               Info.write ~json_path Info.empty;
+               match
+                 Sys.command
+                   (Printf.sprintf "vim -O %s %s" json_path text_dump_path)
+               with
+               | 0 -> (
+                   let info = Info.load ~json_path in
+                   let parts =
+                     [
+                       Option.map string_of_int info.year;
+                       Option.map string_of_int info.month;
+                       Option.map string_of_int info.day;
+                       info.journal;
+                       info.title;
+                       Some ".pdf";
+                     ]
+                     |> List.filter_map (fun x -> x)
+                   in
+                   let name = String.concat "__" parts in
+                   Printf.printf "Computed file name is : %s\n" name;
+                   match
+                     ask_yn
+                       ~prompt:
+                         (Printf.sprintf
+                            "Move PDF file to current dir using above name?")
+                   with
+                   | `Yes -> Ok (Some name)
+                   | `No -> Ok None )
+               | x ->
+                 Printf.printf "Vim exited with return code %d\n" x;
+                 Error () ))
+    in
     match name with
     | Error () -> Error ()
     | Ok None -> aux ~pdf_file_path
@@ -69,31 +93,24 @@ let () =
       if Sys.file_exists path then
         let pdf_file_path =
           if Sys.is_directory path then (
-            print_endline "Specified path is a directory, grabbing the lastest PDF file";
-            let sub_files = Sys.readdir path
-                            |> Array.to_list
-                            |> List.filter_map (fun s ->
-                                if Filename.check_suffix s ".pdf" then
-                                  Some (Filename.concat path s)
-                                else
-                                  None
-                              )
-                            |> List.map (fun s ->
-                                s, Unix.stat s
-                              )
-                            |> List.sort (fun (_, stats1) (_, stats2) ->
-                                let open Unix in
-                                compare stats2.st_mtime stats1.st_mtime
-                              )
-                            |> List.map (fun (s, _) -> s)
+            print_endline
+              "Specified path is a directory, grabbing the lastest PDF file";
+            let sub_files =
+              Sys.readdir path |> Array.to_list
+              |> List.filter_map (fun s ->
+                  if Filename.check_suffix s ".pdf" then
+                    Some (Filename.concat path s)
+                  else None)
+              |> List.map (fun s -> (s, Unix.stat s))
+              |> List.sort (fun (_, stats1) (_, stats2) ->
+                  Unix.(compare stats2.st_mtime stats1.st_mtime))
+              |> List.map (fun (s, _) -> s)
             in
-            List.hd sub_files
-          )
+            List.hd sub_files )
           else path
         in
         match edit_loop ~pdf_file_path with
-        | Ok new_file_name ->
-          Sys.rename pdf_file_path new_file_name
+        | Ok new_file_name -> Sys.rename pdf_file_path new_file_name
         | Error () -> ()
       else print_endline (Printf.sprintf "File \"%s\" does not exist" path)
     with Sys_error _ -> print_endline "Failed to access file"
