8bdaf2e7d124f47e9b0536e86c546f0c2c9950e6
Darren Ldl <darrenldldev@gmail.com>
2019-12-18 11:39:05 +1100

    Adding argument handling

--- src/mvpdfhere.ml
+++ src/mvpdfhere.ml
@@ -1,1 +1,10 @@
-let () = print_endline "Hello, World!"
+let () =
+  let argv = Sys.argv in
+  let arg_count = Array.length argv in
+  if arg_count = 0 then
+    print_endline "No arguments received"
+  else if arg_count > 1 then
+    print_endline "Too many arguments"
+  else
+    let file = argv.(0) in
+    ()
