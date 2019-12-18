8f4e38149f65d7b64e308be45cbe04c9a35284b5
Darren Ldl <darrenldldev@gmail.com>
2019-12-18 17:43:42 +1100

    Progress

---
+++ src/info.ml
@@ -0,0 +1,62 @@
+type t = {
+  year : int option;
+  month : int option;
+  day : int option;
+  journal : string option;
+  title : string option;
+}
+
+let empty = {
+  year = None;
+  month = None;
+  day = None;
+  journal = None;
+  title = None;
+}
+
+let map_int_field_to_json (x : int option) : Yojson.Basic.t =
+  match x with
+  | None -> `Null
+  | Some x -> `Int x
+
+let map_string_field_to_json (x : string option) : Yojson.Basic.t =
+  match x with
+  | None -> `Null
+  | Some x -> `String x
+
+let map_int_field_from_json (x : Yojson.Basic.t) : int option =
+  match x with
+  | `Int x -> Some x
+  | _ -> None
+
+let map_string_field_from_json (x : Yojson.Basic.t) : string option =
+  match x with
+  | `String x -> Some x
+  | _ -> None
+
+let to_json (t : t) : Yojson.Basic.t =
+  let l =
+    [
+      ("year", map_int_field_to_json t.year);
+      ("month", map_int_field_to_json t.month);
+      ("day", map_int_field_to_json t.day);
+      ("journal", map_string_field_to_json t.journal);
+      ("title", map_string_field_to_json t.title);
+    ]
+  in
+  `Assoc l
+
+let of_json (x : Yojson.Basic.t) : t =
+  match x with
+  | `Assoc l ->
+    l
+    |> List.fold_left (fun info (k, v) ->
+        match k with
+        | "year" -> { info with year = map_int_field_from_json v}
+        | "month" -> { info with month = map_int_field_from_json v}
+        | "day" -> { info with day = map_int_field_from_json v}
+        | "journal" -> { info with journal = map_string_field_from_json v}
+        | "title" -> { info with title = map_string_field_from_json v}
+        | _ -> info
+      ) empty
+  | _ -> failwith "Unexpected case"
--- src/mvpdfhere.ml
+++ src/mvpdfhere.ml
@@ -4,15 +4,32 @@ let () =
   if arg_count <= 1 then print_endline "Not enough arguments"
   else if arg_count > 2 then print_endline "Too many arguments"
   else
-    let file = argv.(1) in
+    let path = argv.(1) in
     try
-      if Sys.file_exists file then
+      if Sys.file_exists path then
         let file =
-          if Sys.is_directory file then (
-            print_endline "Specified file is a directory, grabbing lastest file";
-            "" )
-          else file
+          if Sys.is_directory path then (
+            print_endline "Specified path is a directory, grabbing lastest PDF file";
+            let sub_files = Sys.readdir path
+                            |> Array.to_list
+                            |> List.filter (fun s ->
+                                Filename.check_suffix s ".pdf"
+                              )
+                            |> List.map (fun s ->
+                                s, Unix.stat (Filename.concat path s)
+                              )
+                            |> List.sort (fun (_, stats1) (_, stats2) ->
+                                let open Unix in
+                                compare stats2.st_mtime stats1.st_mtime
+                              )
+                            |> List.map (fun (s, _) -> s)
+            in
+            List.hd sub_files
+          )
+          else path
         in
-        ()
-      else print_endline (Printf.sprintf "File \"%s\" does not exist" file)
+        let text_dump_path = Filename.temp_file "mvpdfhere" ".txt" in
+        let json_path = Filename.temp_file "mvpdfhere" ".json" in
+        print_endline file
+      else print_endline (Printf.sprintf "File \"%s\" does not exist" path)
     with Sys_error _ -> print_endline "Failed to access file"
