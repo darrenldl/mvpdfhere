cc39b1f0f4533a93422e45d7cb16b0a931b69980
Darren Ldl <darrenldldev@gmail.com>
2019-12-19 11:35:04 +1100

    Using different sep

--- src/config.ml
+++ src/config.ml
@@ -4,6 +4,6 @@ let words_stay_lowercase = [ "in"; "of"; "for"; "to"; "with" ]
 
 let final_name_sep = "_"
 
-let final_file_name_part_sep = "__"
+let final_file_name_part_sep = "~~"
 
 let date_sep = "-"
